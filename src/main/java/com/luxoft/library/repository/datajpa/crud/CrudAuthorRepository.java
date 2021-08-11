7b160324a9419c109a79b1088241f9d62ade519b
Azatick94 <azburhanov@mail.ru>
2021-08-11 14:18:11 +0300

    datajpa support deletion

--- src/main/java/com/luxoft/library/repository/datajpa/crud/CrudAuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luxoft.library.repository.datajpa.crud;
-
-import com.luxoft.library.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrudAuthorRepository extends JpaRepository<Author, Integer> {
-}
--- src/main/java/com/luxoft/library/repository/datajpa/crud/CrudBookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luxoft.library.repository.datajpa.crud;
-
-import com.luxoft.library.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrudBookRepository extends JpaRepository<Book, Integer> {
-}
